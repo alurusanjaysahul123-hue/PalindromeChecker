ccb503f1c0ba18d3a80b08e6f4d839bac8433f88
alurusanjaysahul123-hue <alurusanjaysahul123@gmail.com>
2026-03-15 21:00:02 +0530

    uc1

---
+++ PalindromeCheckerUC1.java
@@ -0,0 +1,17 @@
+public class PalindromeCheckerUC1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("     Welcome to Palindrome App   ");
+        System.out.println("=================================");
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+        System.out.println("Author           : Console Java Application");
+        System.out.println("---------------------------------");
+        System.out.println("This application checks whether");
+        System.out.println("a given string is a palindrome.");
+        System.out.println("---------------------------------");
+
+    }
+}
